e705310752b42eb0866d9a6265699a947337295f
ptravis92 <paultravis@walcher.org>
2024-07-23 16:43:25 -0500

    Add nav bar & styling updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FeaturedBooksComponent } from './components/featured-books/featured-books/featured-books.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FeaturedBooksComponent],
+  imports: [RouterOutlet, FeaturedBooksComponent, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
